8815546ed098090301c70072d4e27787ac3b2cfd
PRASHANTH K <prashanthprashu.2031@gmail.com>
2026-02-05 18:26:29 +0530

    Create medicalchatbot.py

---
+++ medicalchatbot.py
@@ -0,0 +1,14 @@
+
+import random
+medical_db = {
+
+ "headache": { 
+ "description": "A headache is pain in the head or face.", 
+ "causes": ["Stress", "Tension","Migraine","Dehydration",  
+"Eye strain"], 
+ "remedies": ["Rest in a quiet room", "Apply cold  
+compress", "Take OTC pain relievers", "Stay hydrated"] 
+ },
+
+"vitiligo": {
+"description": "Vitiligo causes loss of skinpigmentation.",
